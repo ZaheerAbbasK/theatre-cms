6282bf7feefb4e4c3f74225778f20c49e40de27c
Your Name <zaheerkundgol29@gmail.com>
2025-10-03 18:44:59 +0530

    cool commit

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-app.use(cors());
+app.use(cors({
+origin : *
+}));
 // Cloudinary config
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
